783287e4fbd1d482d7a7f427da3a3c8fd50a2a3d
Thomas Lo <thomasjinlo@gmail.com>
2016-01-08 11:48:33 -0800

    finish rspec viking

--- spec/warmup_spec.rb
+++ spec/warmup_spec.rb
@@ -9,6 +9,10 @@ describe "warmup tests"  do
       double( size: 3 )
     end
 
+    let(:some_string) {  "tetris"  } 
+
+    let(:loud_string) {  some_string }
+
     describe "#get_shout" do
 
         it "takes a string and returns uppercase" do
@@ -27,5 +31,28 @@ describe "warmup tests"  do
       end
 
     end
+
+  
+
+    describe "#calls_some_methods" do
+
+      it "ensures that upcase! is called" do
+        expect(some_string).to receive(:upcase!).and_return("TETRIS")
+        warmup.calls_some_methods(some_string)
+      end
+
+      it "ensures that reverse! is called" do
+        allow(some_string).to receive(:upcase!).and_return(loud_string)
+        expect(some_string).to receive(:reverse!)
+        warmup.calls_some_methods(some_string)
+      end
+
+      it "returns unrelated string" do
+        allow(some_string).to receive(:upcase!).and_return(loud_string)
+        expect(warmup.calls_some_methods(some_string)).not_to eq("SIRTET")
+      end
+
+    end
+
 end
 
