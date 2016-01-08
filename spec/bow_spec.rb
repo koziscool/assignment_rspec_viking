4f5232b33c4c9072ea0028bfd9c3a47544e4b55f
koziscool <kozjob@yahoo.com>
2016-01-08 16:15:30 -0600

    implemented bow tests

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,36 @@
 # Your code here
+
+require_relative "../lib/weapons/bow"
+
+describe "Bow" do 
+
+    let(:bow) { Bow.new }
+
+    describe "#arrows" do 
+        
+        it "readable" do 
+            expect(bow.arrows).to be_a(Integer)
+        end
+
+        it "initializes to 10" do 
+            expect(bow.arrows).to eq(10)
+        end
+
+        it "initializes to parameter" do 
+            other_bow = Bow.new(15)
+            expect(other_bow.arrows).to eq(15)
+        end
+    end
+
+    describe "#use" do 
+        it "bow use reduces arrows by one" do 
+            expect{ bow.use }.to change(bow, :arrows).by(-1)
+        end    
+
+        it "bow use throws error if out of arrows" do
+            other_bow = Bow.new(0)
+            expect{ other_bow.use }.to raise_error "Out of arrows"
+        end    
+    end
+
+end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,7 @@
 # Your code here
+
+require_relative '../lib/viking'
+
+describe "Viking"  do 
+    
+end
